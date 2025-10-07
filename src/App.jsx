685e56b72c3f13051b7e681439b70432b2b7ef4c
sanjayyy04 <smchauhan2004@gmail.com>
2025-10-07 19:10:19 +0530

    footer file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Homepage from "./homepage";
+import Footer from "./Footer";
 
 function App() {
   return (
     <>
       <h1>simple react app to perform github tutorials..</h1>
+      <Homepage/>
+      <Footer/>
     </>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+  return <h1>allRight are reserved !</h1>;
+}
+
+export default Footer;
